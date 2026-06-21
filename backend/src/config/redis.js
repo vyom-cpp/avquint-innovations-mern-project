a3993b7eca89b48d5c7a564502e2a2a179edb4dd
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-06-21 16:37:24 +0530

    feat(redis): setup Upstash Redis Integration

---
+++ backend/src/config/redis.js
@@ -0,0 +1,9 @@
+import "../config/env.js";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default redis;
