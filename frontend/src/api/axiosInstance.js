6c61e461fabdc64afe00affe02453ccf4da26f98
vyom-cpp <vyom.sutariya0948@gmail.com>
2026-06-23 15:27:52 +0530

    fix: auto logout deleted users of unauthorized requests

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
-
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
@@ -17,6 +15,20 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
 );
 
 export default axiosInstance;
